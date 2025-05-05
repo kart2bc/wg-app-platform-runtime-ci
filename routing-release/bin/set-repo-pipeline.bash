41a4a776a2eee8bb0e090d80039656d66d348063
Geoff Franks <geoff.franks@broadcom.com>
2025-05-05 17:19:43 +0000

    add github actions

--- routing-release/bin/set-repo-pipeline.bash
+++ routing-release/bin/set-repo-pipeline.bash
@@ -2,14 +2,14 @@
 
 set -euo pipefail
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 REPO="$DIR/.."
 FLY_TEAM=wg-arp-networking
 
 main() {
   local pipeline_dir="$(realpath $REPO/pipelines)"
   fly_login
-  fly_pipeline routing-release -f "${pipeline_dir}/routing-release.yml" \
+  fly_pipeline routing-actions -f "${pipeline_dir}/routing-actions.yml" \
     -f "$REPO/index.yml" \
     -f "$REPO/../shared/helpers/ytt-helpers.star"
 }
